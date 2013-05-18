195c6201912a91fd8b0f5569a82572a68ab414ae
Ricky Lam <ricky@Ricky-Lams-MacBook-Pro.local>
2013-05-18 17:43:55 +0800

    Hong's App first commit with refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HongApp::Application.config.secret_token = '1922c2b081374490026954b3e0b1f48d1c728c91e9121dcbb8af0175292b83e20b6a7bfccfe38d5dcec9a501f840504a62940df7d2ead34a71b3e01dba8a15b1'
